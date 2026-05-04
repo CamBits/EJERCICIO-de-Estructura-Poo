38f2b4a013de876409ea67054a4546dba6c53ba5
DESKTOP-F6UU0NK\Nitropc <camilaguerrero703@gmail.com>
2026-05-04 10:09:03 +0200

    initial scripts

---
+++ Assets/Scripts/Climabicho.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climabicho : MonoBehaviour
+{
+
+
+    //Atributes
+
+    //Public
+    public string nombre;
+    public EstadoEmocional estado;
+
+
+    //Private
+    private float energia;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EventoClimatico.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventoClimatico : MonoBehaviour
+
+{
+
+
+    //Atributes
+
+    //Public
+    public string tipoEvento;
+    public Climabicho origen;
+
+
+    //Private
+    private float  duracion ;
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
