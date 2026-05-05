7b05df7301e233ccec05aa6d9a53539cb03f374f
DESKTOP-F6UU0NK\Nitropc <camilaguerrero703@gmail.com>
2026-05-05 12:40:45 +0200

    estadoemocional casi terminado pero funcionable en climabicho

--- Assets/Scripts/Climabicho.cs
+++ Assets/Scripts/Climabicho.cs
@@ -18,15 +18,21 @@ public class Climabicho : MonoBehaviour
 
 
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        estado.estadoActual();
+       
+
+
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/EstadoEmocional.cs
+++ Assets/Scripts/EstadoEmocional.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class EstadoEmocional : MonoBehaviour
@@ -8,23 +10,59 @@ public class EstadoEmocional : MonoBehaviour
     //Atributes
 
     //Public
-
-    public string tipo;
+   
     public string efectoVisual;
 
 
     //Private
+    private string[] tipo = { "Feliz", "Triste", "Enojado", "Neutral"};
+
     private float multiplicadorPoder;
 
-    // Start is called before the first frame update
-    void Start()
+   public void estadoActual()
     {
-        
+        int lista = Random.Range(0, tipo.Length);
+        string emocion = tipo[lista];
+
+        switch (emocion)
+        {
+
+            case "Feliz":
+
+                efectoVisual = "una luz radiante";
+
+              break;
+
+            case "Triste":
+
+                efectoVisual = "niebla azulada";
+
+                break;
+
+            case "Enojado":
+
+                efectoVisual = "nubes negras";
+
+                break;
+
+
+            case "Neutral":
+
+                efectoVisual = "que no hay ningun cambio";
+
+                break;
+
+        }
+
+        print("El climabicho esta " + emocion + "(ves " + efectoVisual + ")");
+
+       // Debug.Log("Emocion: " + emocion);
+      // Debug.Log("Efecto visual: " + efectoVisual);
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
